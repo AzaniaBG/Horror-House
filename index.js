3818bf070e90c982429e94d4acc52a8c9e11cefe
AzaniaBG <azaniazbaker@gmail.com>
2020-01-12 11:37:31 -0800

    added function stubs

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+
+//format query parameters
+    function formatQueryParams() {
+
+    //format YouTube parameters for SEARCH endpoint
+
+    //format YouTube parameters for VIDEOS endpoint
+
+    }
+//call the YouTube API to retrieve video IDs 
+    function getVideoIDs() {
+
+    }
+//call the YouTube API to retrieve video players/trailers
+    function getVideos() {
+
+    }
+    
+//display information related to search results 
+    function displayMovieInfo() {
+    //display movie name and year
+
+    //display poster image for movie result(s)
+
+    //display rating
+
+    //display additional information
+    
+    }
+//embed video for user to play
+    function embedVideo() {
+
+    }
+//list similar movies based on search results
+    function displaySimilarMovies() {
+    //display movie name and year
+
+    //display movie information
+
+    }
+//watch for the form submission
+    function watchForm() {
+
+    //capture the values of the user's input and pass those values to the GET function
+
+    }
+//ACTIVATE APP--call j$ and pass in a callback function to run when the page loads
+$(watchForm)
