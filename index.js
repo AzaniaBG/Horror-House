0ced7313362b444585761bae37586d0568777b47
AzaniaBG <azaniazbaker@gmail.com>
2020-01-13 12:26:59 -0800

    added function stubs with pseudo code

--- index.js
+++ index.js
@@ -22,8 +22,21 @@ const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
     //format YouTube parameters for VIDEOS endpoint
 
     }
+//find similar movies and list results according to maxResults specified
+    function getSimilarMovies(query, maxResults) {
+        
+
+        // const params = {
+        //     q: query,
+        //     maxNum: maxResults,
+        //     part: "snippet",
+        //     type: "video", 
+        //     fields: fieldsParams,
+        //     key: YouTubeKey
+        // }
+    }
 //GET movie info
-    function getMovieSnippets(query, maxResults) {
+    function getMovieSnippets(query) {
         let fieldsParams = "items(snippet, id/videoId)"
 
         const params = {
@@ -56,7 +69,7 @@ const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
         
     }
 
-//call the YouTube API to retrieve video players/trailers
+//call the YouTube API to retrieve video players/trailers and video info
     function getVideos(vidId) {
         let fieldParams = "items(etag, player)"
         const params = {
@@ -71,7 +84,15 @@ console.log(`vidURL is ${vidURL}`)
         fetch(vidURL).then(response => response.json()).then(responseJson => console.log(responseJson));
 
     }
+//embed video for user to play
+    function embedVideo() {
+        //get video player details
+        //set video player size
+        //include video controls
+        //display message if video unavailable
+        //display message if video player not supported
 
+    }
     function displayTrailers(vidId) {
 //console.log(`displayTrailers ran`)
         
@@ -79,7 +100,7 @@ console.log(`vidURL is ${vidURL}`)
 
     }
     
-//display information related to search results 
+//display information related to search results for one movie
     function displayMovieInfo(titles, images) {
     //display movie name and year
         console.log(`<h3>${titles}</h3>`)
@@ -92,35 +113,36 @@ console.log(`vidURL is ${vidURL}`)
         $(".js-movie-img").html(`<img src="${defaultImg}">`);
     //display rating
 
-    //display additional information
+    //display additional information (e.g., articles/reviews)
     
     }
-//embed video for user to play
-    function embedVideo() {
 
-    }
 //list similar movies based on search results
     function displaySimilarMovies() {
     //display movie name and year
 
     //display movie information
 
+    //user can click on result and view information with displayMovieInfo function
+
     }
-//watch for the form submission
-    function watchForm() {
-        
-        
-    //when a user submits a search; get the value
+//watch the form and get user input
+    function watchForm() {   
+    //when a user searches for one movie, get the value, include that value in GET request
         $("form").on("submit", event => {
             event.preventDefault();
             let searchTerm = $("#js-one-movie-search").val();
-            getMovieSnippets(searchTerm, 5);
-console.log(`searchTerm is ${searchTerm}`)
+    //capture the values of the user's input and pass those values to the GET function
+            getMovieSnippets(searchTerm);
+//console.log(`searchTerm is ${searchTerm}`)
             }
+    //when a user searches for similar movies and max Results, get the value, include those values in GET request
+
+    //capture user's input values and pass them to the GET function: getSimilarMovies(query, maxResults)
 
         )
 
-    //capture the values of the user's input and pass those values to the GET function
+    
 
     }
     
