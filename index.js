9e5363ebf16c0136785bca0073e07d199f7fcc7d
AzaniaBG <azaniazbaker@gmail.com>
2020-01-14 10:24:45 -0800

    split functions into to: 1. get ID and 2. get images

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 //store API keys in global variables to access for API calls
 const tmdbKey = "b81d09aa5f188c95ba4dc2e4336459b4"
-const YouTubeKey = "AIzaSyD9L4wcH4JuIUXlNkavwNEQl-kH2_MsIOg"
+//const YouTubeKey = "AIzaSyD9L4wcH4JuIUXlNkavwNEQl-kH2_MsIOg"
 
 //save API base URLs to modify according to search
-const tmdbURL = "https://api.themoviedb.org/3/"
+const tmdbURL = "https://api.themoviedb.org/3/search/"
 //config for ID: https://api.themoviedb.org/3/configuration?api_key=b81d09aa5f188c95ba4dc2e4336459b4
-const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
+//const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
 //https://www.googleapis.com/youtube/v3/videos?&part=snippet&fields=items(snippet)&key=AIzaSyD9L4wcH4JuIUXlNkavwNEQl-kH2_MsIOg
 //test variables
 
@@ -15,7 +15,7 @@ const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
 //format query parameters
     function formatQueryParams(params) {
     //create an array of keys from the PARAMS `object` argument and, for each key, create a formatted key-pair value string
-        const searchQueryItems = Object.keys(params).map(key => `${key}=${params[key]}`);
+        const searchQueryItems = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`);
     //return a string of each key-pair value separated by &s
         return searchQueryItems.join("&");
 
@@ -38,98 +38,136 @@ const YouTubeURL = "https://www.googleapis.com/youtube/v3/"
         //     key: YouTubeKey
         // }
     }
+//GET movie images
+    function getMovieId(query) {
+
+        const params = {
+            query: query,
+            language: "en-US",
+            api_key: tmdbKey,
+            page: 1,
+            include_adult: false,
+            //external_source: "imdb_id",
+            //append_to_response: "images",
+        }
+        //let multiRequest = "&append_to_response=images"
+        const queryString = formatQueryParams(params);
+        const idURL = tmdbURL + `movie?`+ queryString;
+        //GET movie ID with fetch
+        fetch(idURL).then(response => response.json()).then(responseJson => getMovieImages(responseJson));      
+    }
+
+    function getMovieImages(responseJson) {
+console.log(`responseJson is`);
+console.log(responseJson);
+        let movieID = responseJson.results[0]["id"];
+
+        const params = {
+            language: "en-US",
+            api_key: tmdbKey,
+            page: 1,
+            include_adult: false,
+            //external_source: "imdb_id",
+            append_to_response: "images",
+        }
+        //GET movie image with movie ID
+        const queryString = formatQueryParams(params);
+        const idURL = tmdbURL + `movie/`+ `${movieID}?` + queryString;
+console.log(`idURL is ${idURL}`)
+        //fetch(idURL).then(response => response.json()).then(responseJson => console.log(responseJson));
+    }
 //GET movie info
-    function getMovieSnippets(query) {
-        let fieldsParams = "items(snippet, id/videoId)"
-        let trailerQuery = query + ",trailer"
-console.log(`trailerQuery is ${trailerQuery}`)
-        const params = {
-            q: trailerQuery,
-            part: "snippet",
-            type: "video", 
-            fields: fieldsParams,
-            key: YouTubeKey
-        }
-        const queryString = formatQueryParams(params);
-        const snippetURL = YouTubeURL + `search?` + queryString;
-console.log(`snippetURL is ${snippetURL}`)
-        fetch(snippetURL).then(response =>response.json()).then(responseJson => {
-            displayMovieInfo(responseJson);
-            displaySimilarMovies(responseJson);
-            getVideos(responseJson);
+//     function getMovieSnippets(query) {
+//         let fieldsParams = "items(snippet, id/videoId)"
+//         let trailerQuery = query + ",trailer"
+// console.log(`trailerQuery is ${trailerQuery}`)
+//         const params = {
+//             q: trailerQuery,
+//             part: "snippet",
+//             type: "video", 
+//             fields: fieldsParams,
+//             key: YouTubeKey
+//         }
+//         const queryString = formatQueryParams(params);
+//         const snippetURL = YouTubeURL + `search?` + queryString;
+// console.log(`snippetURL is ${snippetURL}`)
+//         fetch(snippetURL).then(response =>response.json()).then(responseJson => {
+//             displayMovieInfo(responseJson);
+//             displaySimilarMovies(responseJson);
+//             getVideos(responseJson);
 
-        //responseJson returns ITEMS array, containing ID and snippet objects 
-console.log(responseJson)
-        //for each item in the ITEMS Array, return an array of string IDs, titles, and images
-        })
+//         //responseJson returns ITEMS array, containing ID and snippet objects 
+// console.log(responseJson)
+//         //for each item in the ITEMS Array, return an array of string IDs, titles, and images
+//         })
        
-    }
+//     }
 
-//call the YouTube API to retrieve video players/trailers and video info
-    function getVideos(vidId) {
-        let fieldParams = "items(etag, player)"
-        const params = {
-            id: vidId,
-            part: "player",
-            fields: fieldParams,
-            key: YouTubeKey
-        }
-        const queryString = formatQueryParams(params);
-        const vidURL = YouTubeURL + `videos?` + queryString;
-console.log(`vidURL is ${vidURL}`)
-        fetch(vidURL).then(response => response.json()).then(responseJson => console.log(responseJson));
+// //call the YouTube API to retrieve video players/trailers and video info
+//     function getVideos(vidId) {
+//         let fieldParams = "items(etag, player)"
+//         const params = {
+//             id: vidId,
+//             part: "player",
+//             fields: fieldParams,
+//             key: YouTubeKey
+//         }
+//         const queryString = formatQueryParams(params);
+//         const vidURL = YouTubeURL + `videos?` + queryString;
+// console.log(`vidURL is ${vidURL}`)
+//         fetch(vidURL).then(response => response.json()).then(responseJson => console.log(responseJson));
 
-    }
-//embed video for user to play
-    function embedVideo() {
-        //get video player details
-        //set video player size
-        //include video controls
-        //display message if video unavailable
-        //display message if video player not supported
+//     }
+// //embed video for user to play
+//     function embedVideo() {
+//         //get video player details
+//         //set video player size
+//         //include video controls
+//         //display message if video unavailable
+//         //display message if video player not supported
 
-    }
-    function displayTrailers(vidId) {
-//console.log(`displayTrailers ran`)
+//     }
+//     function displayTrailers(vidId) {
+// //console.log(`displayTrailers ran`)
         
-        return `<video id="${vidId}" src="${vidId}" class="js-trailer">Trailer: ${vidId}</video>`;
+//         return `<video id="${vidId}" src="${vidId}" class="js-trailer">Trailer: ${vidId}</video>`;
 
-    }
+//     }
     
-//display information related to search results for one movie
-    function displayMovieInfo(responseJson) {
-console.log(responseJson)
-//         responseJson.items.map(item => {
-//             let vidIds = item.id["videoId"];
-//             getVideos(vidIds);
-//             let titles = item.snippet["title"];
+// //display information related to search results for one movie
+//     function displayMovieInfo(responseJson) {
+// console.log(responseJson)
+// //         responseJson.items.map(item => {
+// //             let vidIds = item.id["videoId"];
+// //             getVideos(vidIds);
+// //             let titles = item.snippet["title"];
             
-//             let images = item.snippet.thumbnails;
-//             displayMovieInfo(titles, images);       
-// //console.log(`titles is ${titles}`)
-//             });
-    //display movie name and year
-console.log(`<h3>${titles}</h3>`)    
-    //display rating
+// //             let images = item.snippet.thumbnails;
+// //             displayMovieInfo(titles, images);       
+// // //console.log(`titles is ${titles}`)
+// //             });
+//     //display movie name and year
+// console.log(`<h3>${titles}</h3>`)    
+//     //display rating
 
-    //display additional information (e.g., articles/reviews)   
-    }
+//     //display additional information (e.g., articles/reviews)   
+//     }
 
-//list similar movies based on search results
-    function displaySimilarMovies() {
-        responseJson.items.map(item => {
-        let vidIds = item.id["videoId"];
-        getVideos(vidIds);
-        let titles = item.snippet["title"];
+// //list similar movies based on search results
+//     function displaySimilarMovies() {
+//         responseJson.items.map(item => {
+//         let vidIds = item.id["videoId"];
+//         getVideos(vidIds);
+//         let titles = item.snippet["title"];
         
-        let images = item.snippet.thumbnails;
-        displayMovieInfo(titles, images);       
-//console.log(`titles is ${titles}`)
-        });
-    //display movie name and year
-    //display movie information
-    //user can click on result and view information with displayMovieInfo function
-    }
+//         let images = item.snippet.thumbnails;
+//         displayMovieInfo(titles, images);       
+// //console.log(`titles is ${titles}`)
+//         });
+//     //display movie name and year
+//     //display movie information
+//     //user can click on result and view information with displayMovieInfo function
+//     }
 //watch the form and get user input
     function watchForm() {   
     //when a user searches for one movie, get the value, include that value in GET request
@@ -137,7 +175,8 @@ console.log(`<h3>${titles}</h3>`)
             event.preventDefault();
             let searchTerm = $("#js-one-movie-search").val();
     //capture the values of the user's input and pass those values to the GET function
-            getMovieSnippets(searchTerm);
+        getMovieId(searchTerm);
+            //getMovieSnippets(searchTerm);
 //console.log(`searchTerm is ${searchTerm}`)
             }
     //when a user searches for similar movies and max Results, get the value, include those values in GET request
